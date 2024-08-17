9870f00d7e90b19c26b01074db96ed55bd2c43f2
samanzari <samanzari71@gmail.com>
2024-08-17 16:39:50 +0500

    Portflio

---
+++ src/data/constants.js
@@ -0,0 +1,190 @@
+export const Bio = {
+  name: "Saman Zari",
+  roles: [
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Saman-Zari",
+  resume:
+    "https://drive.google.com/file/d/1Q_j5WireJnfmR8Wx0QlVJdiIoV1EBTQJ/view",
+  linkedin: "https://www.linkedin.com/in/saman-zari1/",
+  twitter: "#",
+  insta: "https://www.instagram.com/zarirajpoot21/",
+  facebook: "https://www.facebook.com/people/Saman-Zari/pfbid02xiAU2kwQAzZ5aQTA5dYkmQE586Ju1RZyymiCMv1sbMxJ5daytSzbCRYm8xRTkH5Xl/?mibextid=ZbWKwL",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+
+    ],
+  },
+  
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCEj_V6_aIHeHJk9LAEq1kk5Bs7bjAzb82qw&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+      
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1166690750/85f69649-5387-44c2-ba45-81ae13812e36-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  
+  {
+    id: 0,
+    img: "https://infinene.com.pk/assets/img/logo/logo-1.png",
+    role: "Frontend Engineer Intern",
+    company: "INFINEN Tech",
+    date: "May 2024 - Present",
+    desc: "Working on the frontend of the web application using ReactJS.",
+    skills: [
+      "ReactJS",
+      "Tailwind Css",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://www.dukan.pk/dtemp/ptImg1720163464-37086066628602.png",
+    role: "Front End Developer",
+    company: "Knowledge Stream",
+    date: "Jan 2024 - Mar 2024",
+    desc: "Creative and detail-oriented front-end developer specializing in crafting responsive and user-friendly web interfaces.",
+    skills: [
+      "ReactJS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "BootStrap",
+      "JQuery",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.vu.edu.pk/apply/bower_components/icons/admission-logo.jpg",
+    school: "Virtual University of Pakistan",
+    date: "2018 - 2023",
+    grade: "3.12 CGPA",
+    desc: "I have completed a Bachelor's degree in Computer Science and Engineering at Virtual University. I graduated with a CGPA of 3.12 and have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://scontent.flhe7-2.fna.fbcdn.net/v/t39.30808-1/301490164_461890135952441_2474348018059989883_n.jpg?stp=dst-jpg_p200x200&_nc_cat=105&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=wJIevcKDLFEQ7kNvgFP78Iv&_nc_ht=scontent.flhe7-2.fna&oh=00_AYDPjhls3p4b6dUBjYjkLltmJCvML97m9s5NDKYWcrdHgw&oe=66B3FD76",
+    school: "DPS Safdarabad",
+    date: "2016 - 2018",
+    grade: "70%",
+    desc: "I completed my class 12 high school education at DPS,Safarabad , where I studied Science with Computer Science.",
+    degree: "(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://scontent.flhe7-1.fna.fbcdn.net/v/t39.30808-1/359491085_692952242876516_303485732570288063_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=auL7Bsvk7lIQ7kNvgFLhq6F&_nc_ht=scontent.flhe7-1.fna&oh=00_AYA3paGfYAQ_MWVtPdE_gibU82aeT-V6cRMuP9221eEG_g&oe=66B40A05",
+    school: "Govt. High School Safdarabad",
+    date: "2014 - 2016",
+    grade: "65%",
+    desc: "I completed my class 10 education at Govt. High School Safdarabad, where I studied Science subject.",
+    degree: "(X) ",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 0,
+    title: "Live X",
+    
+    description:
+      "LiveX is Pakistan's top digital learning platform, empowering you to master skills for confidence and future success..",
+    image:
+      "http://media.livex.tech/Slider/93b3497b-1264-4dad-a10d-a6d76618a5cf_Summer-Bootcamp-Slider---(3690x1350).jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+  
+    github: "https://livex.tech",
+    webapp: "https://livex.tech",
+    
+  },
+  {
+    id: 1,
+    title: "INFINENE Tech",
+    
+    description:
+      "\nInfinene delivers top-tier tech services and expertise, driving innovation and excellence in tech-enabled solutions.",
+    image:
+      "https://infinene.com.pk/assets/img/logo/logo-1.png",
+    tags: [
+      "React", "NextJs", "MySql"
+    ],
+    
+    github: "https://infinene.com.pk/index.html",
+    webapp: "https://infinene.com.pk/index.html",
+  },
+  
+  
+  
+ 
+  
+ 
+  
+  
+];
+
